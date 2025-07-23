ee420b5d18f2c5b4a725919baa15110ed2a17e41
Adrian Guerrero <adrianjguerrero87@gmail.com>
2025-07-23 12:03:44 -0400

    created helper to most used functions

---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+export function camelCaseToParamCase(camelCaseString) {
+  return camelCaseString.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+}
+
+export function applyStylesFromStore(store) {
+  for (const key in store.customStyles) {
+    const { value, selector } = store.customStyles[key]
+    console.log(value, selector, key)
+    document
+      .querySelector(selector || ':root')
+      .style.setProperty(`--${camelCaseToParamCase(key)}`, value)
+  }
+}
