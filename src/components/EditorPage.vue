d7f0c66273906c7dfc791960d861ee33a9e3d963
Adrian Guerrero <adrianjguerrero87@gmail.com>
2025-07-22 20:19:29 -0400

    more stable base editor

--- src/components/EditorPage.vue
+++ src/components/EditorPage.vue
@@ -15,18 +15,50 @@ function setValueonStore(e, stateProperty, elementSelector = ':root') {
 }
 </script>
 <template>
-  <div>
-    <input type="color" @input="setValueonStore($event, 'mainColor')" />
-    <input type="color" @input="setValueonStore($event, 'textColor')" />
-    <input
-      type="color"
-      @input="setValueonStore($event, 'jumbotronBackgroundColor', 'header .jumbotron')"
-    />
-    <input
-      type="color"
-      @input="setValueonStore($event, 'jumbotronTextColor', 'header .jumbotron')"
-    />
+  <div class="editor-container">
+    <div>
+      <label for="main-color">Color de fondo principal</label>
+      <input id="main-color" type="color" @input="setValueonStore($event, 'mainColor')" />
+    </div>
+    <div>
+      <label for="text-color">Color de texto principal</label>
+      <input id="text-color" type="color" @input="setValueonStore($event, 'textColor')" />
+    </div>
+    <div>
+      <label for="jumbotron-background-color">Color de fondo Header</label>
+      <input
+        id="jumbotron-background-color"
+        type="color"
+        @input="setValueonStore($event, 'jumbotronBackgroundColor', 'header .jumbotron')"
+      />
+    </div>
+    <div>
+      <label for="jumbotron-text-color">Color de texto header</label>
+      <input
+        id="jumbotron-text-color"
+        type="color"
+        @input="setValueonStore($event, 'jumbotronTextColor', 'header .jumbotron')"
+      />
+    </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.editor-container {
+  display: flex;
+  top: 0;
+  flex-direction: column;
+  color: white;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  padding: 1em;
+}
+
+.editor-container > div {
+  margin-top: 1em;
+  display: flex;
+}
+.editor-container > div > input {
+  margin-left: auto;
+}
+</style>
