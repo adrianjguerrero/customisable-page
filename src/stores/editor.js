ace461ff7316493ad3382749fe0f229b2b2d200c
Adrian Guerrero <adrianjguerrero87@gmail.com>
2025-07-23 11:09:57 -0400

    refactor to be able to save on localstorage easily

--- src/stores/editor.js
+++ src/stores/editor.js
@@ -3,21 +3,25 @@ import { defineStore } from 'pinia'
 
 export const useEditorStore = defineStore('editor', () => {
   const showEditor = ref(true)
-  const mainColor = ref('')
-  const textColor = ref('')
-  const jumbotronBackgroundColor = ref('')
-  const jumbotronTextColor = ref('')
+  const customStyles = ref({
+    mainColor: '',
+    textColor: '',
+    jumbotronBackgroundColor: '',
+    jumbotronTextColor: '',
+  })
 
   function toggleShowEditor() {
     showEditor.value = !showEditor.value
   }
 
+  function setCustomStyles(newStyles) {
+    customStyles.value = { ...customStyles.value, ...newStyles }
+  }
+
   return {
     toggleShowEditor,
     showEditor,
-    mainColor,
-    textColor,
-    jumbotronBackgroundColor,
-    jumbotronTextColor,
+    customStyles,
+    setCustomStyles,
   }
 })
